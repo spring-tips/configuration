bfe4d3c5c9e2ab927bd3efe91e279686ea42e111
Josh Long <josh@joshlong.com>
2020-04-22 04:13:59 -0700

    init

--- configuration/bin/init-vault.sh
+++ configuration/bin/init-vault.sh
@@ -1,2 +1,4 @@
+export VAULT_ADDR="http://localhost:8200"
+export VAULT_SKIP_VERIFY=true
+export VAULT_TOKEN=00000000-0000-0000-0000-000000000000
 vault kv put secret/bootiful message-from-vault-server="Hello Vault"
-#vault kv put secret/bootiful/cloud username=clouduser password=cloudpassword
